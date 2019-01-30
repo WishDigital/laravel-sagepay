cc98a28b9115fdb395bebb1761670fb5f965c541
Sledmore <sledmore2012@gmail.com>
2019-01-30 05:12:08 +0000

    Support Laravel 5.x

--- src/SagePayServiceProvider.php
+++ src/SagePayServiceProvider.php
@@ -4,20 +4,18 @@ namespace Kofikwarteng\LaravelSagepay;
 
 use Illuminate\Support\ServiceProvider;
 
-class SagePayServiceProvider extends ServiceProvider
+class SagePayServiceProvider extends \Illuminate\Support\ServiceProvider
 {
     /**
-     * Bootstrap the application services.
+     * Perform post-registration booting of services.
      *
      * @return void
      */
     public function boot()
     {
-      //copy the package's config file to laravel's config directory
-      $this->publishes([
-    __DIR__.'/Config/sagepay.php' => config_path('sagepay.php'),
-        ], 'config');
-
+        $this->publishes([
+            __DIR__.'/Config/sagepay.php' => config_path('sagepay.php'),
+        ]);
     }
 
     /**
@@ -27,11 +25,12 @@ class SagePayServiceProvider extends ServiceProvider
      */
     public function register()
     {
-      //get the config from the config file, that way, we can have access to it in our Model
-      $this->mergeConfigFrom( __DIR__.'/Config/sagepay.php', 'sagepay');
+        $this->mergeConfigFrom(
+            __DIR__.'/Config/sagepay.php', 'sagepay'
+        );
 
         $this->app->singleton('laravelsagepay', function ($app) {
-            return new SagePay($app);
+            return new SagePay();
         });
     }
 }
